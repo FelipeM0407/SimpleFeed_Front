ba5ab27152fc3bb6037b342e99d2057e59471db1
FelipeM0407 <felipemoreirasilva97@gmail.com>
2025-01-13 21:13:06 -0300

    feat: enhance field editing dialog with new layout, improved button placement, and updated styles

--- src/app/features/forms/components/form-edit/form-edit.component.ts
+++ src/app/features/forms/components/form-edit/form-edit.component.ts
@@ -13,11 +13,12 @@ import { FieldTypes } from '../../models/FieldTypes';
 import { MatDialog } from '@angular/material/dialog';
 import { EditFieldDialogComponent } from './edit-field-dialog/edit-field-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-form-edit',
   standalone: true,
-  imports: [MatButtonModule, MatMenuModule, MatTabsModule, MatIconModule, CommonModule, MatDialogModule],
+  imports: [MatCardModule, MatButtonModule, MatMenuModule, MatTabsModule, MatIconModule, CommonModule, MatDialogModule],
   templateUrl: './form-edit.component.html',
   styleUrls: ['./form-edit.component.scss'],
 })
