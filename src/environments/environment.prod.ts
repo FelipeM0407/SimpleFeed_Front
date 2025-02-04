98e7cf1ce49a527547d99b263f896926a4349867
FelipeM0407 <felipemoreirasilva97@gmail.com>
2025-02-04 18:18:47 -0300

    feat: update front-end URL in environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'https://gestaonex.com.br/api',
-    frontUrl: 'https://simplefeed-bucket.s3.us-east-1.amazonaws.com/api'
+    frontUrl: 'https://simple-feed-front.vercel.app/'
   };
   
