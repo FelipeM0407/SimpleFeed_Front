f39af407c7cef0b75c486c39371087a916c99713
FelipeM0407 <felipemoreirasilva97@gmail.com>
2025-04-21 17:13:18 -0300

    finalização da usabilidade da expiração do formulário

---
+++ src/app/features/feedback-submission/components/feedback-form/expired-form-dialog/expired-form-dialog.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  standalone: true,
+  selector: 'app-expired-form-dialog',
+  templateUrl: './expired-form-dialog.component.html',
+  styleUrls: ['./expired-form-dialog.component.scss'],
+  imports: [MatIconModule]
+})
+export class ExpiredFormDialogComponent {
+  
+}
