6c566ee9413fadaa6facd34206ca74f1e2c7ee43
FelipeM0407 <felipemoreirasilva97@gmail.com>
2025-05-10 22:25:10 -0300

    remocao menu lateral

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -8,37 +8,19 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 import { MatTooltipModule} from '@angular/material/tooltip';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MediaMatcher } from '@angular/cdk/layout';
-import { MatListModule } from '@angular/material/list';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
   standalone: true,
-  imports: [CommonModule, MatListModule, MatSidenavModule, MatTooltipModule, RouterModule, MatDividerModule, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
+  imports: [MatTooltipModule, RouterModule, MatDividerModule, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
 })
 export class DashboardComponent implements OnInit {
   userName: string | null = null;
   userEmail: string | null = null;
-  mobileQuery: MediaQueryList;
-  fillerNav = Array.from({length: 5}, (_, i) => `Nav Item ${i + 1}`);
-  fillerContent = Array.from(
-    {length: 15},
-    () =>
-      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-  );
 
-  constructor(private router: Router, private authService: AuthService, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) { 
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-
-  }
+  constructor(private router: Router, private authService: AuthService, changeDetectorRef: ChangeDetectorRef) {}
 
   ngOnInit() {
     this.userName = this.authService.getUserName();
