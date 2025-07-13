45f46dc37c5e4b85b15cad75f195c01c9b05b5ba
FelipeM0407 <felipemoreirasilva97@gmail.com>
2025-07-13 00:09:06 -0300

    ajuste dominio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://app.simplefeed.onrender.com/api',
+    apiUrl: 'https://simplefeed.onrender.com/api',
     frontUrl: 'https://www.app.simplefeed.com.br/'
   };
   
