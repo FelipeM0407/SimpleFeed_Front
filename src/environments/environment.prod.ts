a858137375106177631a2268791291e67e500273
FelipeM0407 <felipemoreirasilva97@gmail.com>
2025-07-13 00:25:47 -0300

    Fix frontUrl in production environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'https://simplefeed.onrender.com/api',
-    frontUrl: 'https://www.app.simplefeed.com.br/'
+    frontUrl: 'https://app.simplefeed.com.br/'
   };
   
